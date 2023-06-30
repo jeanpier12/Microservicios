893b7d64cfe1b518ac47796714e99dffa2dcfb95
jeanpier <jeanpierincato@gmail.com>
2023-06-29 19:39:28 -0500

    AgregandoHeurekaA categorias

--- micro-servicio-Categorias/src/main/java/certus/edu/pe/MicroservicioCategoriasApplication.java
+++ micro-servicio-Categorias/src/main/java/certus/edu/pe/MicroservicioCategoriasApplication.java
@@ -2,7 +2,8 @@ package certus.edu.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicioCategoriasApplication {
 
