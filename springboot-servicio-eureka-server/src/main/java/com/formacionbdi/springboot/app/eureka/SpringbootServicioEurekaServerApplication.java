36087fdf84c8768b8fd3f205db7dd8ef60846ef1
jeanpier <jeanpierincato@gmail.com>
2023-07-01 17:14:54 -0500

    servidor-eureka

--- springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
+++ springboot-servicio-eureka-server/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplication.java
@@ -1,16 +1,15 @@
-package certus.edu.pe.eureka;
+package com.formacionbdi.springboot.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//anotacion para trabajar con eureka server
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringBootServicioEurekaSeverApplication {
+public class SpringbootServicioEurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootServicioEurekaSeverApplication.class, args);
+		SpringApplication.run(SpringbootServicioEurekaServerApplication.class, args);
 	}
 
 }
--- springboot-servicio-eureka-server/src/test/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
+++ springboot-servicio-eureka-server/src/test/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package certus.edu.pe.eureka;
+package com.formacionbdi.springboot.app.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootServicioEurekaSeverApplicationTests {
+class SpringbootServicioEurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
