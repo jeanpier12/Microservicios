53c7af006c2e077d8604f337e7d3ddb1c1f477ca
jeanpier <jeanpierincato@gmail.com>
2023-07-04 22:38:52 -0500

    "Subiendo-APY-gateway"

--- springboot-servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
+++ springboot-servicio-config-server/src/main/java/com/formacionbdi/springboot/app/config/SpringbootServicioConfigServerApplication.java
@@ -2,7 +2,7 @@ package com.formacionbdi.springboot.app.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+///import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /// descomentar @EnableDiscoveryClient
